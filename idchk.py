f7b76b21bb9dc0175f8d262e5d4b1d3a7d5b29f6
buptxiaomiao <buptwjh@outlool.com>
2016-03-19 00:26:52 +0800

    idchk.py can check if a string be an identifier(not keywords.)

--- idchk.py
+++ idchk.py
@@ -7,22 +7,28 @@
 #########################################################################
 #!/usr/bin/env python2.7
 import string
-al = string.letters+'_'
+import keyword
+
+alpha = string.letters+'_'
 nums = string.digits
+keys=keyword.kwlist
 
-print 'Welcome to the Identifier Checker v1.0'
-print 'Testees must be at least 2 chars long'
-myinput = raw_input('Identifier to test?')
+print 'Welcome to the Identifier Checker v1.1'
+print ' Identifier must be at least 1 chars long'
+print ' Spaces in start or endding will be ignored'
+print ' First char must be char.'
+myinput = raw_input('Identifier to test?\n').strip()
 
-if len(myinput)>1:			#do not forget ::::::::
-	if myinput[0] not in al:
-		print '''invalid:first symbol must be 
-				alphabetic'''
+if len(myinput) >= 1:			#do not forget ::::::::
+	if myinput[0] not in alpha:
+		print '''Invalid:first symbol must be alphabetic'''
 	else:
 		for otherchar in myinput[1:]:
-			if otherchar not in al+nums:
-				print '''invalid: remaing
-				symbols must be alphanumberic'''
+			if otherchar not in alpha+nums:
+				print '''Invalid: remaing symbols must be alphanumberic'''
 				break
 		else:#excute when for is excute without break
-			print 'okay as an identifier'
+			if myinput not in keyword.kwlist:
+				print '%s is okay as an identifier'%myinput
+			else :
+				print 'Sorry ,%s'%myinput,'is an keyword.'
